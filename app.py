001026d8bb22d0064b0a2d76cdb066c14b4e2e0e
Charles Yuan <charlesyuan59@gmail.com>
2022-06-15 23:18:50 -0400

    Added PUT and DELETE

--- app.py
+++ app.py
@@ -23,6 +23,25 @@ waifu_args.add_argument("name", type=str, required=False)
 waifu_args.add_argument("anime", type=str, required=False)
 waifu_args.add_argument("rank", type=int, required=False)
 
+
+def query(waifu_id=None, waifu_name=None, waifu_rank=None):
+    '''
+    Just a helper function for querying.
+    Since id, name, rank are all unique, allow for querying by these options.
+    '''
+    if waifu_id:
+        result = WaifuEntry.query.filter_by(id=waifu_id).first()
+        return result
+    elif waifu_name:
+        result = WaifuEntry.query.filter_by(name=waifu_name).first()
+        return result
+    elif waifu_rank:
+        result = WaifuEntry.query.filter_by(name=waifu_rank).first()
+        return result
+    else:
+        return None
+
+
 class WaifuEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary_key=True makes id a unique identifier
     name = db.Column(db.String(100), nullable=False) # nullable=False means you must enter something
@@ -57,10 +76,10 @@ class WaifuList(Resource):
         # Check if it's a repeat entry (for ID and name)
         id_result = WaifuEntry.query.filter_by(id=args["id"]).first()
         if id_result:
-            abort(409, message="ID taken...")
+            abort(409, message="ID taken.")
         name_result = WaifuEntry.query.filter_by(name=args["name"]).first()
         if name_result:
-            abort(409, message="Waifu already exists within database...")
+            abort(409, message="Waifu already exists within database.")
 
         # Move all entries with ranks equal to or lower down by 1 to avoid tie ranks
         existing_results = WaifuEntry.query.filter(WaifuEntry.rank >= args["rank"]).all()
@@ -83,34 +102,58 @@ class WaifuList(Resource):
     def get(self):
         '''
         Since id, name, rank are all unique, allow for querying by these options.
-        I can't think of a better way than 6 conditional statements.
-        Feel free to make a PR if you do.
+        I wanted to reuse this method, so I put it in a separate function.
         '''
         args = waifu_args.parse_args()
 
-        if args["name"] and args["anime"] and args["rank"]:
-            result = WaifuEntry.query.filter_by(name=args["name"], anime=args["anime"], rank=args["rank"]).first()
-            return result
-        elif args["name"] and args["anime"]:
-            result = WaifuEntry.query.filter_by(name=args["name"], anime=args["anime"]).first()
-            return result
-        elif args["anime"] and args["rank"]:
-            result = WaifuEntry.query.filter_by(anime=args["anime"], rank=args["rank"]).first()
-            return result
-        elif args["name"] and args["rank"]:
-            result = WaifuEntry.query.filter_by(name=args["name"], rank=args["rank"]).first()
-            return result
-        elif args["name"]:
-            result = WaifuEntry.query.filter_by(name=args["name"]).first()
-            return result
-        elif args["anime"]:
-            result = WaifuEntry.query.filter_by(name=args["anime"]).first()
-            return result
-        elif args["rank"]:
-            result = WaifuEntry.query.filter_by(name=args["rank"]).first()
+        result = query(waifu_id=args["id"], waifu_name=args["name"], waifu_rank=args["rank"])
+        if result:
             return result
         else:
-            abort(409, message="Cannot query without name, anime, or rank parameters. Please enter at least one.")
+            abort(409, message="Cannot query without id, name, or rank parameters. Please enter at least one.")
+            
+    
+    @marshal_with(resource_fields)
+    def put(self):
+        '''
+        PUT is basically just update.
+        Yes, this does allow for duplicate ranks, but it's necessary for re-ordering purposes.
+        Also, you MUST search by ID, otherwise you can't actually change anything.
+            The query parameters this time are the changed parameters.
+            The ID stays the same.
+        '''
+        args = waifu_args.parse_args()
+
+        result = WaifuEntry.query.filter_by(id=args["id"]).first()
+        if result:
+            # Check one by one for changes, don't change if param is None or ID
+            if args["name"]:
+                result.name = args["name"]
+            if args["anime"]:
+                result.anime = args["anime"]
+            if args["rank"]:
+                result.rank = args["rank"]
+            db.session.commit() # Always remember to commit when making changes!
+            return result
+        else:
+            abort(409, message="Cannot use PUT without ID parameter.")
+    
+    
+    @marshal_with(resource_fields)
+    def delete(self):
+        '''
+        DELETE is basically GET, but you remove the entry at the end.
+        Use the same helper function.        
+        '''
+        args = waifu_args.parse_args()
+
+        result = query(waifu_id=args["id"], waifu_name=args["name"], waifu_rank=args["rank"])
+        if result:
+            db.session.delete(result)
+            db.session.commit()
+            return result
+        else: # If there aren't adequate query params, or if it's been deleted already
+            abort(409, message="Cannot find entry. Please check input parameters, or whether the entry still exists.")
 
 
 if __name__ == "__main__":
